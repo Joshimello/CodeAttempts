0a4a6f6c9d70c8795c0e8d3596b06c2ad7eda2a1
Joshimello <joshualeanjw@gmail.com>
2022-09-28 10:20:05 +0800

    28/9/2022

--- nthuoj/2022 - Hu/13590 - smart little kai.c
+++ nthuoj/2022 - Hu/13590 - smart little kai.c
@@ -1,24 +1,27 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 int main(void) {
-  
+  while (1) {
     int num, arr[32];
-    scanf("%d", &num);
+    if (scanf("%d", &num) == EOF) {
+      break;
+    }
 
     int i = 0, n = num;
-    while(n != 0) {
+    while (n != 0) {
       arr[i] = n % 10;
       n /= 10;
       i++;
     }
 
     int sum = 0;
-    for(int j = i - 1; j > -1; j--)
+    for (int j = i - 1; j > -1; j--)
       sum += pow(arr[j], i);
 
-    if(num == sum)
+    if (num == sum)
       printf("Yes it is.\n");
     else
       printf("No it is not.\n");
+  }
 }
